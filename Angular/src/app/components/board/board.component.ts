7d5f8f349fface3458a2d8cdf466b828cbabe12e
joshhauser <none>
2019-05-11 23:00:36 +0200

    responsive layout | scrolling

--- Angular/src/app/components/board/board.component.ts
+++ Angular/src/app/components/board/board.component.ts
@@ -8,8 +8,8 @@ import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag
 })
 export class BoardComponent implements OnInit {
 
-  tab = ['tab','test','lolllll','okokok'];
-  tab2 = ['tzaeraab','terrest','lolllrearll','okokerazeraezrok'];
+  tab = ['tab','test','lolllll','okokok','tzaeraab','terrest','lolllrearll','okokerazeraezrok', 'tzaeraab','terrest','lolllrearll','okokerazeraezrok'];
+  tab2 = [1,2,3,4,5,6,7,8,9,87,87,5];
   constructor() { }
 
   ngOnInit() {
